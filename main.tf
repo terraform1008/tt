a3d8478a290fad7e75b3ed4cfee269745baed535
Arun <terraform1008@outlook.com>
2024-10-28 14:53:36 +0000

    checking

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-
-    backend "azurerm" {
-    resource_group_name  = "tf-demo-rg"
-    storage_account_name = "tfdemo10008"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-
 }
 
 resource "azurerm_resource_group" "tfdemo" {
